a74583c490c0fb079c7235bef8babaf28b7b97fa
David Calabrese <69607423+davidtcalabrese@users.noreply.github.com>
2021-05-09 20:17:53 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ The purpose of this function is to show / hide password text
 -----------------------------------------------------------------*/
 
 const togglePasswordVisibility = () => {
-  let pwInput = document.querySelector("#password"); // select password input field 
-  pwInput.type === "password"        // if input type is set to password
-    ? (pwInput.type = "text")        // switch to text (visible)
-    : (pwInput.type = "password");   // otherwise switch to password (hidden)
+  let passwordInput = document.querySelector("#password"); // select password input field 
+  passwordInput.type === "password"        // if input type is set to password
+    ? (passwordInput.type = "text")        // switch to text (visible)
+    : (passwordInput.type = "password");   // otherwise switch to password (hidden)
 };
